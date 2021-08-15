03aa18005d30e55c72b7fae8b40d2b32ce0bfc16
Roy Xiong <roy.xiong.1@gmail.com>
2021-08-14 20:27:36 -0500

    Return location information from tweet endpoint

--- src/view/index.js
+++ src/view/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { getLocationDetails } = require('../api/geocoding');
 const { getTweets } = require('../api/twitter');
-const {  isHashtagValid } = require('../util/twitter');
 
 const app = express();
 app.use(express.json());
@@ -11,38 +10,54 @@ app.get('/api/twitter', async (req, res) => {
     
     const { hashtag } = req.query;
 
-    if (!hashtag || !isHashtagValid(hashtag)) {
+    if (!hashtag) {
         res.status(400);
         res.json({
             title: 'Invalid Request',
-            detail: 'Hashtag is missing or invalid.',
+            detail: 'Please specify a hashtag',
         });
         return;
     }
 
-    const tweets = await getTweets(hashtag);
+    const jsonData = await getTweets(hashtag);
 
     // Error returned from Twitter API
-    if (tweets.title && tweets.detail) {
-        if (tweets.status) {
-            res.status(tweets.status);
+    if (jsonData.title && jsonData.detail) {
+        if (jsonData.status) {
+            res.status(jsonData.status);
         } else {
             res.status(400);
         }
         res.json({
-            title: tweets.title,
-            detail: tweets.detail,
+            title: jsonData.title,
+            detail: jsonData.detail,
         });
         return;
     }
 
     // No results
-    if (tweets.meta.result_count === 0) {
+    if (jsonData.meta.result_count === 0) {
         res.json({ data: [] });
         return;
     }
 
-    res.json({ data: tweets });
+    const tweets = jsonData.data;
+    const { users } = jsonData.includes;
+
+    const locationDetails = await getLocationDetails(
+        users.map(user => user.location).filter(location => location)
+    );
+
+    res.json({
+        data: tweets.map(tweet => {
+            const author = users.find(user => user.id === tweet.author_id);
+            return {
+                author,
+                tweet,
+                author_location: locationDetails[author.location],
+            }
+        }),
+    });
 });
 
 const PORT = process.env.PORT || 3000;
